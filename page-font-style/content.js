afeba31d9de56f01f4520ff7699f5cac743c63ac
Wender Z. Xavier <wender.zacariasxavier@gmail.com>
2021-03-15 16:11:56 -0300

    Fix content.js wrong method name

--- page-font-style/content.js
+++ page-font-style/content.js
@@ -1,6 +1,6 @@
 chrome.runtime.sendMessage({ todo: "showPageAction" })
 
-chrome.rruntime.onMessage.addListener(function(request, sender, sendResponse){
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     if(request.todo == 'changeColor'){
         let addColor = '#' + request.clickedColor;
         $('.api').css('color', addColor);
